3d0fbd45d7ab480e7a5ad8bad274b4d814df5836
Douglas <Brown>
2019-10-07 06:50:41 -0400

    adding axios for api calls

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,7 +1,21 @@
-// Global app controller
 
-import num from './test'
-// require("@babel/polyfill");
+import axios from 'axios';  // importing npm packages dirctly into the app
 
-const x = 23;
-console.log(`I imported ${num} from another module! Variable x is ${x}.`);
+async function getResults(query) {
+    const key = '986b523ea59363c4493ecf43555b9efa';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    }
+    catch(error) {
+        alert(error);
+    }
+}
+
+getResults('pizza');
+
+
+
+// 986b523ea59363c4493ecf43555b9efa
+// https://www.food2fork.com/api/search
